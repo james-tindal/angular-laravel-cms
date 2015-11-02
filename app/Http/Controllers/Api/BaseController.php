db56abafaa83cb547f6947be9b3f6a3a016a5fb6
james-tindal <10291002+james-tindal@users.noreply.github.com>
2015-11-02 00:27:13 +0000

    Replace Api folder

---
+++ app/Http/Controllers/Api/BaseController.php
@@ -0,0 +1,10 @@
+<?php namespace HLS\Http\Controllers\Api;
+
+
+use Dingo\Api\Routing\Helpers;
+use HLS\Http\Controllers\Controller;
+
+class BaseController extends Controller
+{
+    use Helpers;
+}
