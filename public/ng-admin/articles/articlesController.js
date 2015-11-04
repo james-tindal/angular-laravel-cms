901c5359e1b44b232ac9de5d3728eb359fdc5c54
james-tindal <10291002+james-tindal@users.noreply.github.com>
2015-11-04 12:22:19 +0000

    Can delete articles from admin app

--- public/ng-admin/articles/articlesController.js
+++ public/ng-admin/articles/articlesController.js
@@ -18,12 +18,14 @@
       archived: 0
     };
     vm.processing = false;
+    vm.processingDestroy = false;
     vm.error;
     vm.success;
     vm.disabled = false;
 
     vm.update = update;
     vm.create = create;
+    vm.destroy = destroy
 
 
 
@@ -47,7 +49,7 @@
       $http.put('api/articles/' + $stateParams.id, vm.article).success(function(response) {
         vm.processing = false;
         vm.error = false;
-        vm.success = true;
+        vm.success = 'Article updated!';
         $timeout(function() {
           vm.success = false;
         }, 6000);
@@ -71,16 +73,39 @@
       $http.post('api/articles', vm.article).success(function(response) {
         vm.processing = false;
         vm.error = false;
-        vm.success = true;
+        vm.success = 'Article created!';
         console.log(response);
 
         $timeout(function() {
           $state.go('articles')
-        }, 800);
+        }, 2000);
       }).error(function(error) {
         console.log(error);
 
         vm.processing = false;
+        vm.error = error;
+        vm.success = false;
+      });
+    }
+
+    function destroy() {
+      vm.processingDestroy = true;
+
+      $http.delete('api/articles/' + $stateParams.id).success(function(response) {
+        $('.modal').modal('hide');
+        vm.processingDestroy = false;
+        vm.error = false;
+        vm.success = 'Article deleted!';
+        console.log(response);
+
+        $timeout(function() {
+          $state.go('articles')
+        }, 2000);
+      }).error(function(error) {
+        console.log(error);
+        $('.modal').modal('hide');
+
+        vm.processingDestroy = false;
         vm.error = error;
         vm.success = false;
       });
