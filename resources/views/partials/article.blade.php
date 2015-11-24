965a938ed759e4624597d671246688650921846b
james-tindal <10291002+james-tindal@users.noreply.github.com>
2015-11-24 22:19:05 +0000

    refactored article views with view composer

---
+++ resources/views/partials/article.blade.php
@@ -0,0 +1,14 @@
+<section>
+    <aside>
+        {!! $image !!}
+    </aside>
+    <header>
+        <h4>{{ $title }}</h4>
+
+        <p>
+            <span {!! $withCategories !!}>{{ $published_at->format('d-m-Y') }}</span>
+            {!! $categories !!}
+        </p>
+    </header>
+    <p>{{ $brief }} <a href="{{ url('news/' . $slug) }}">Read more</a></p>
+</section>
