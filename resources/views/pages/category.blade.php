b2fa486f2ef28010af77788f860b3899a8fbe977
james-tindal <10291002+james-tindal@users.noreply.github.com>
2015-11-24 22:30:35 +0000

    applied refactoring to category route

--- resources/views/pages/category.blade.php
+++ resources/views/pages/category.blade.php
@@ -4,32 +4,15 @@
     <h1>{{ $name }}</h1>
     <div class="latest-news">
         @foreach($articles as $article)
-            <section>
-                @unless (is_null($article->image_url))
-                    <aside>
-                        <img src="{{ $article->image_url }}" alt="">
-                    </aside>
-                @endunless
-                <header>
-                    <h4>{{ $article->title }}</h4>
-
-                    <p>
-                        <span @if(sizeof($article->categories) > 0)class="with-categories"@endif>{{ $article->published_at->format('d-m-Y') }}</span>
-                        @foreach($article->categories as $category)
-                            <a href="{{ url('category') }}/{{ $category->name }}">{{ $category->name }}</a>,
-                        @endforeach
-                    </p>
-                </header>
-                <p>{{ $article->brief }} <a href="{{ url('news') }}/{{ $article->slug }}">Read more</a></p>
-            </section>
+            @include('partials.article')
         @endforeach
     </div>
 
-    @if(sizeof($categories) > 0)
+    @if($categories)
         <nav class="news-categories">
             <ul>
                 @foreach($categories as $category)
-                    <li><a href="{{ url('category') }}/{{ $category->name }}">{{ $category->name }}</a></li>
+                    <li><a href="{{ url('category/' . $category->slug) }}">{{ $category->name }}</a></li>
                 @endforeach
             </ul>
         </nav>
