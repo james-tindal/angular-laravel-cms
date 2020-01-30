3a2c6dcbfbe53c1c855bb5eccad47c15fd353082
james-tindal <10291002+james-tindal@users.noreply.github.com>
2020-01-30 12:07:46 +0000

    Configured for Elastic Beanstalk

--- config/database.php
+++ config/database.php
@@ -60,10 +60,11 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'host'      => env('RDS_HOSTNAME', 'localhost'),
+            'port'      => env('RDS_PORT', 3306)
+            'database'  => env('RDS_DB_NAME', 'forge'),
+            'username'  => env('RDS_USERNAME', 'forge'),
+            'password'  => env('RDS_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
